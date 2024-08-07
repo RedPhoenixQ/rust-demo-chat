ed5b985de510311a976f767258b5d72c7c6441b9
RedPhoenixQ <steve.dark.03@gmail.com>
2024-08-07 18:26:10 +0200

    feat: get messages for specific channels

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -1,42 +1,50 @@
-use axum::{extract::State, routing, Router};
+use axum::{
+    extract::{Path, State},
+    routing, Router,
+};
 use sqlx::query;
+use uuid::Uuid;
 
 use crate::{base_tempalte, utils::uuid_to_date, AppState};
 
 pub fn router() -> Router<AppState> {
     Router::new().route(
-        "/",
-        routing::get(|State(state): State<AppState>| async move {
-            let messages = query!(
-                "SELECT m.id, m.content, m.updated, m.author, u.name as author_name 
+        "/channels/:channel_id",
+        routing::get(
+            |State(state): State<AppState>, Path(channel_id): Path<Uuid>| async move {
+                let messages = query!(
+                    r#"SELECT m.id, m.content, m.updated, m.author, u.name as author_name 
                 FROM messages AS m
-                JOIN chat_users AS u ON u.id = m.author"
-            )
-            .fetch_all(&state.db)
-            .await
-            .unwrap();
+                JOIN chat_users AS u ON u.id = m.author
+                WHERE m.channel = $1"#,
+                    channel_id,
+                )
+                .fetch_all(&state.db)
+                .await
+                .unwrap();
 
-            base_tempalte(maud::html!(
-                div class="btn" {"Hello, World!"}
-                div {
-                    @for msg in messages {
-                        .chat.chat-start {
-                            .chat-header {
-                                (msg.author_name)
-                                span.text-xs.opacity-50 {
-                                    (uuid_to_date(msg.id).unwrap().to_string())
+                base_tempalte(maud::html!(
+                    div class="btn" {"Hello, World!"}
+                    div {
+                        @for msg in messages {
+                            .chat.chat-start {
+                                .chat-header {
+                                    (msg.author_name)
+                                    span.text-xs.opacity-50 {
+                                        (uuid_to_date(msg.id).unwrap().to_string())
+                                    }
                                 }
-                            }
-                            .chat-bubble {
-                                (msg.content)
-                            }
-                            .chat-footer {
-                                (msg.updated.to_string())
+                                .chat-bubble {
+                                    (msg.content)
+                                }
+                                .chat-footer {
+                                    (msg.updated.to_string())
+                                }
                             }
                         }
                     }
-                }
-            ))
-        }),
+                ))
+            },
+        ),
     )
 }
